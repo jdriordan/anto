e8636e67043004cd233a516e96efcac463e2e2fd
Danny Riordan <dannyriordan@gmail.com>
2015-09-12 03:22:28 +0100

    added code for finding possible moves, knight's tour

--- chess.hs
+++ chess.hs
@@ -3,7 +3,7 @@ import Data.Char
 data Pos = Pos (Int, Int)
 data PieceType = P | N | B | R | Q | K
 data Side = White | Black
-data Piece = Piece (Side, PieceType, Pos)
+data Piece = Piece (Side, PieceType, Pos) -- record syntax?
 data Board = Board [Piece]
 
 main = print "lol"
@@ -13,7 +13,10 @@ setup = Board [Piece (White, P,Pos (1,1))]
 --instance Read Pos where
 readPos [file,rank] = Pos (ord file - 96, digitToInt rank)
 
+--instance Show Pos where
+showPos (Pos (x,y)) = [chr (x+96), intToDigit y]
 
+-- need to add special moves
 legal :: Piece -> Pos -> Bool
 legal piece@(Piece (s, t, Pos (oldX,oldY))) pos@(Pos (newX,newY))=
     inBounds newX && inBounds newY &&
@@ -33,3 +36,9 @@ legal piece@(Piece (s, t, Pos (oldX,oldY))) pos@(Pos (newX,newY))=
 
 swap :: Piece->PieceType->Piece
 swap (Piece (s,t,p)) t' = Piece(s,t',p)
+
+-- Possible moves for a knight from pos
+wholeBoard = [Pos (x,y) | x<-[1..8], y<-[1..8]]
+nmoves pos = map showPos $ filter (legal$Piece(White,N,readPos pos)) wholeBoard
+next' (p:ps) = [p':p:ps|p'<-nmoves p]
+next = concatMap next' 
