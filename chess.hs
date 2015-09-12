b2cc7fc8b09ead3f4c593b0010a5a8bd9a94de20
Danny Riordan <dannyriordan@gmail.com>
2015-09-12 20:41:47 +0100

    some refactoring

--- chess.hs
+++ chess.hs
@@ -1,50 +1,31 @@
 import Data.Char
 import Data.Array
 
-data Pos = Pos (Int, Int)
+type Pos = (Int, Int) 
 data PieceType = P | N | B | R | Q | K deriving (Show, Read, Eq, Ord)
-data Side = White | Black
-data Piece = Piece {side :: Side, ptype :: PieceType, pos :: Pos} 
-data Board = Board [Piece]
+data Side = White | Black deriving (Eq, Show)
+data Piece = Piece {side :: Side, ptype :: PieceType, pos :: Pos} deriving Show
+type Board = Array Pos Piece 
 
--- instance Show Board where
-showBoard (Board ps) = 
-    let board = ar//[(getPos piece, icon piece) | piece<-ps] in
-    [[board!(y,x)|y<-[1..8]]|x<-[1..8]]
-    where 
-        ar = listArray ((1,1),(8,8)) $ cycle "."
-        getPos (Piece  _ _ (Pos pos)) = pos
-        icon (Piece s t _) = 
-            case s of 
-                White -> fst
-                Black -> snd
-            $
-            case t of
-                P -> ('♙','♟')
-                N -> ('♘','♞')
-                B -> ('♗','♝')
-                R -> ('♖','♜')
-                Q -> ('♕','♛')
-                K -> ('♔','♚')
+-- There's now some cponfusion between Board and [Piece]
 
 main = print "lol"
 
 setup = Board  $
-    [Piece White P (Pos (x,2))|x<-[1..8]] ++
-    [Piece Black P (Pos (x,7))|x<-[1..8]] ++
-    zipWith (Piece White) (map (\l->read[l]) "RNBQKBNR") [Pos (i,1)|i<-[1..8]] ++
-    zipWith (Piece Black) (map (\l->read[l]) "RNBKQBNR") [Pos (i,8)|i<-[1..8]]
+    [Piece White P (x,2)|x<-[1..8]] ++
+    [Piece Black P (x,7)|x<-[1..8]] ++
+    zipWith (Piece White) (map (\l->read[l]) "RNBQKBNR") [(i,1)|i<-[1..8]] ++
+    zipWith (Piece Black) (map (\l->read[l]) "RNBKQBNR") [(i,8)|i<-[1..8]]
      
-
 --instance Read Pos where
-readPos [file,rank] = Pos (ord file - 96, digitToInt rank)
+readPos [file,rank] = (ord file - 96, digitToInt rank)
 
 --instance Show Pos where
-showPos (Pos (x,y)) = [chr (x+96), intToDigit y]
+showPos ((x,y)) = [chr (x+96), intToDigit y]
 
 -- need to add special moves
 legal :: Piece -> Pos -> Bool
-legal piece@(Piece s t (Pos (oldX,oldY))) pos@(Pos (newX,newY))=
+legal piece@(Piece s t (oldX,oldY)) pos@(newX,newY)=
     inBounds newX && inBounds newY &&
     dX+dY/=0 && -- no non-moves!
     case t of
@@ -60,11 +41,36 @@ legal piece@(Piece s t (Pos (oldX,oldY))) pos@(Pos (newX,newY))=
         dX = abs $ oldX-newX
         dY = abs $ oldY-newY
 
+move :: Board -> Piece -> Pos -> Board
+-- move :: 
+
 swap :: Piece->PieceType->Piece
 swap (Piece s t p) t' = Piece s t' p
 
--- Possible moves for a knight from pos
-wholeBoard = [Pos (x,y) | x<-[1..8], y<-[1..8]]
-nmoves pos = map showPos $ filter (legal$Piece White N (readPos pos)) wholeBoard
-next' (p:ps) = [p':p:ps|p'<-nmoves p]
-next = concatMap next' 
+wholeBoard = [(x,y) | x<-[1..8], y<-[1..8]]
+possibleMoves piece = filter (legal piece) wholeBoard
+
+
+blankBoard = listArray ((1,1),(8,8)) $ cycle "."
+boardUpdate board update = board//[(getPos piece, icon piece) | piece<-getPieces board]
+
+-- instance Show Board where
+showBoard (Board ps) = 
+
+    [[()!(y,x)|y<-[1..8]]|x<-[1..8]]
+    where 
+        getPos (Piece  _ _ pos) = pos
+        icon (Piece s t _) = 
+            case s of 
+                White -> fst
+                Black -> snd
+            $
+            case t of
+                P -> ('♙','♟')
+                N -> ('♘','♞')
+                B -> ('♗','♝')
+                R -> ('♖','♜')
+                Q -> ('♕','♛')
+                K -> ('♔','♚')
+
+
