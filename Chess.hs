649076acd38f240f01b4b1ce9e35ecba2c23cd67
Danny Riordan <dannyriordan@gmail.com>
2016-12-13 15:00:12 +0000

    almost parsing moves, still thinks they're illegal

--- Chess.hs
+++ Chess.hs
@@ -27,15 +27,47 @@ play = do
 humanMove s b = do
   printBoard b
   putStrLn $ show s ++ " to move."
+  putStrLn "Enter move, or hit return for choices"
+  moveString <- getLine
+  if moveString == ""
+    then choiceMove s b
+    else parsedMove s b moveString
+
+parsedMove s b moveString =
+  let parse = parseMove' moveString in
+    case parse of
+      Nothing -> humanMove s b
+      Just m ->
+        return $ uncurry (doMove b) $ specificMove s b m
+
+
+specificMove :: Side -> Board -> (PieceType,Pos) -> Move
+specificMove s b move@(p,(f,r)) =
+  let candidates = filter (\x-> side x==s && ptype x==p) $ getPieces b
+      piece = head $ filter (\x-> legal startBoard (x,(1,3))) candidates --TODO unsafe
+  in
+    (piece,(f,r))
+{-
+
+parsedMove s b = do
+  putStrLn "No parse, try again."
+  humanMove s b
+parsedMove s b (Just move) =
+  if legal move then return move
+  else
+    putStrLn "Illegal move, try again."
+    humanMove s b
+-}
+choiceMove s b =  do
   let moves = getMoves b s
--- mapM_ putStrLn [show n++". "++ agnMove move | move <- moves]
+--mapM_ putStrLn [show n++". "++ agnMove move | move <- moves]
   mapM_ putStrLn [show n++". "++ agnMove (moves !! n) |n<-[0..length moves-1]]
   moveNumber <- getLine
-  return $ uncurry (move b) (moves !! read moveNumber)
+  return $ uncurry (doMove b) (moves !! read moveNumber)
 
 aiMove s b = do
   putStrLn $ "\n Computer plays " ++ agnMove bestMove ++ ".\n"
-  return $ uncurry (move b) bestMove
+  return $ uncurry (doMove b) bestMove
   where
     bestMove = bestMoveBy evaluateBoard s b
 
@@ -57,38 +89,32 @@ showPos (x,y) = [chr (x+96), intToDigit y]
 agnMove :: Move -> String
 agnMove (Piece _ t _, pos) = (if t==P then "" else show t) ++ showPos pos
 
---parseMove :: [Char] -> Maybe Move
-
--- return a function asking for a side and a position to constuct a move
-parseMove :: String -> Side -> Pos -> Maybe Move
-parseMove [pieceType,file,rank] side pos = do -- I am master of the maybe monad
+parseMove' [pieceType,file,rank] = do -- I am master of the maybe monad
   p <- readMaybe $ return pieceType
   f <- readFile' file
   r <- readMaybe $ return rank
-  return (Piece side p pos,(f,r))
-
-parseMove' [pieceType,file,rank] = do -- I am master of the maybe monad
-  p <- readMaybe $ return pieceType
-  f <- readMaybe $ return file
-  r <- readMaybe $ return rank
-  return [p,f,r]
+  return (p,(f,r))
+parseMove' [{-pawn,-} file, rank] =
+  parseMove' ['P',file,rank]
+parseMove _ = trace "couldn't parse move" Nothing
 
 --TODO need to add special moves
-legal :: Board -> Piece -> Pos -> Bool
-legal board piece@(Piece s t (oldX,oldY)) newPos@(newX,newY)
+legal :: Board -> Move -> Bool
+legal board (piece@(Piece s t (oldX,oldY)), newPos@(newX,newY))
   =
   inBounds newX && inBounds newY -- don't go out of bounds
   &&
     dX+dY/=0 -- no non-moves!
   &&
   case t of -- gotta move according to the rules
+              --trace ("dX,f1,hE: " ++ concatMap show [dX==0, forward 1,not hitEnemy ]++ show piece ++ show newPos ) False ||
     P -> (dX==0           && forward 1 && not hitEnemy)  || -- normal move -- TODO allow promotion
          (dX==1           && forward 1 &&     hitEnemy)  || -- kill
          (oldY`elem`[2,7] && forward 2 && not hitEnemy)     -- first move --TODO allow en passant
     N -> (dX, dY) `elem`[(1,2),(2,1)]
     B -> dX==dY
     R -> dX==0 || dY==0
-    Q -> any (\t'-> legal board (swap piece t') newPos) [B,R]
+    Q -> any (\t'-> legal board (swap piece t', newPos)) [B,R]
     K -> not (dX>1 || dY>1)       --TODO castling
   &&
   (null inbetweens -- can't pass through other pieces
@@ -108,14 +134,14 @@ legal board piece@(Piece s t (oldX,oldY)) newPos@(newX,newY)
       EQ -> [a]
     firstHit = head inbetweens
     hitEnemy =  case board!newPos of
-      Nothing -> otherwise
+      Nothing -> False
       Just victim -> side victim /= s
     forward n =
       oldY`pm`n==newY
 
-move :: Board -> Piece -> Pos -> Board
-move board piece to =
-  if legal board piece to then
+doMove :: Board -> Piece -> Pos -> Board
+doMove board piece to =
+  if legal board (piece, to) then
   board//[
     (pos piece,Nothing),
     (to, Just $ piece {pos=to})]
@@ -130,7 +156,7 @@ swap (Piece s t p) t' = Piece s t' p
 wholeBoard = [(x,y) | x<-[1..8], y<-[1..8]]
 
 --TODO this is the slowest and most naive way to do this
-possibleMoves board piece = filter (legal board piece) wholeBoard
+possibleMoves board piece = filter (\pos-> legal board (piece,pos)) wholeBoard
 
 blankBoard :: Board
 blankBoard = listArray ((1,1),(8,8)) $ repeat Nothing  -- Johnny Tighlips
@@ -165,7 +191,7 @@ icon (Piece s t _) =
     K -> ('♔','♚')
 
 justMove b =
-  map (\p ->move b p (head $ possibleMoves b p))
+  map (\p ->doMove b p (head $ possibleMoves b p))
     $ getPieces b
 
 getMoves b s =
@@ -173,14 +199,14 @@ getMoves b s =
 
 getSide b s = filter (\x-> side x == s) $ getPieces b
 
-nextBoards b s = map (uncurry $ move b) $ getMoves b s
+nextBoards b s = map (uncurry $ doMove b) $ getMoves b s
 
 -- the hard part
 
 type Evaluator = Side -> Board -> Int
 
 bestMoveBy :: Evaluator -> Side -> Board -> Move
-bestMoveBy eval s b = maximumBy (comparing $ eval s . uncurry (move b)) $ getMoves b s
+bestMoveBy eval s b = maximumBy (comparing $ eval s . uncurry (doMove b)) $ getMoves b s
 
 evaluateBoard :: Evaluator
 evaluateBoard = iterate evalPredictive evalMaterial !! 2 -- any more than three takes forever
@@ -194,7 +220,7 @@ evalMaterial s b =
   where
     sign me it = if it==me then 1 else (-1)
 
-evalPredictive e s b = e s $ uncurry (move b) $ bestMoveBy e s b
+evalPredictive e s b = e s $ uncurry (doMove b) $ bestMoveBy e s b
 
 -- Misc
 
